d7d0539f6967e12f07eb6781c71887efd11a55dd
cubyto <emiliocube165@gmail.com>
2023-12-28 03:12:05 -0500

    Palette od the web page implemented, and new icons

--- src/public/js/dashboard.js
+++ src/public/js/dashboard.js
@@ -1,6 +1,6 @@
 const sidebar = document.querySelector('.sidebar')
 const closeBtn = document.querySelector('#btn')
-const searchBtn = document.querySelector('.bx-search')
+const searchBtn = document.querySelector('.fi-rr-search')
 
 closeBtn.addEventListener('click', () => {
   sidebar.classList.toggle('open')
@@ -15,8 +15,8 @@ searchBtn.addEventListener('click', () => { // Sidebar open when you click on th
 // following are the code to change sidebar button(optional)
 function menuBtnChange () {
   if (sidebar.classList.contains('open')) {
-    closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right')// replacing the iocns class
+    closeBtn.classList.replace('fi-rr-menu-burger', 'fi-rr-bars-staggered')// replacing the iocns class
   } else {
-    closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu')// replacing the iocns class
+    closeBtn.classList.replace('fi-rr-bars-staggered', 'fi-rr-menu-burger')// replacing the iocns class
   }
 }
